a4c1a9f6efb0108f4fa7e81c1068a583f8e1fa11
jeongyeonKimm <wjddus9132@gmail.com>
2023-02-28 01:46:05 +0900

    Webtoon APP

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB), 
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          background: const Color(0xFFE7626C),
-        ), 
-      ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
